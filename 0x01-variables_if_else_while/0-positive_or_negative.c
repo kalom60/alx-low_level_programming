390dc8c33961e91f2da82e9944916015efc29c4d
kalom60 <kaleabtesfaye00@gmail.com>
2021-08-27 04:43:56 -0700

    change variable i to n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - determine if a number is positive or negative
  *
- * Description: program will assign a random number to the variable i each time
+ * Description: program will assign a random number to the variable n each time
  * it is executed.
  *
  * Return: Always 0 (Success)
@@ -13,16 +13,16 @@
 
 int main(void)
 {
-	int i;
+	int n;
 
 	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i == 0)
-		printf("%d is zero\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
-		printf("%d is positive\n", i);
+		printf("%d is positive\n", n);
 
 	return (0);
 }
