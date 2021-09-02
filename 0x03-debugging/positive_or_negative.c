2fa0ca7a53d8658d8b9f9734f24ee54f1062f36e
kalom60 <kaleabtesfaye00@gmail.com>
2021-09-02 02:42:27 -0700

    add positive or negative c file

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - determine if a number is positive or negative
+ *
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is positive\n", i);
+	}
+}
