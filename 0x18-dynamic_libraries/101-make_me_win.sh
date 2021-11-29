01642cc4a192ada8eb7cbc2614a2b8268c1c2581
kalom60 <39136564+kalom60@users.noreply.github.com>
2021-11-30 02:31:14 +0300

    Fix error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/kalom60/alx-low_level_programming/master/0x18-dynamic_libraries/random.so
+wget -P /tmp/ https://raw.github.com/kalom60/alx-low_level_programming/tree/master/0x18-dynamic_libraries/random.so
 export LD_PRELOAD=/tmp/random.so
