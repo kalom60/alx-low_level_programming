f13e0f9990aec3ac32131f9388a29e6a69ec9aba
kalom60 <kaleabtesfaye00@gmail.com>
2021-12-17 06:52:53 -0800

    header file for the program

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,5 +44,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 /* 4-hash_table_get.c */
 char *hash_table_get(const hash_table_t *ht, const char *key);
+/* 5-hash_table_print.c */
+void hash_table_print(const hash_table_t *ht);
 
 #endif
